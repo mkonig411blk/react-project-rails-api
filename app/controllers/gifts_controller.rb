b0092f299db76aa0c155c053d303c56b51367da8
mkonig411blk <margot.konig@blackrock.com>
2019-10-08 13:33:59 -0500

    added index to gifts controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,2 +1,15 @@
 class GiftsController < ApplicationController
+    def index
+        gifts = Gift.all
+        render json: gifts, except: [:created_at, :updated_at]
+    end
+
+    # def show
+    #     gift = Gift.find_by(id: params[:id])
+    #     if gift
+    #         render json: gifts, except: [:created_at, :updated_at]
+    #     else
+    #         render json: {message: "Gift not found."}
+    #     end
+    # end
 end
