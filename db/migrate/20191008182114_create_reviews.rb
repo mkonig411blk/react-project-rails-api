29cad7b66dd2bce94f9049aa60eb630dbd023847
mkonig411blk <margot.konig@blackrock.com>
2019-10-08 13:31:35 -0500

    db:migrate & db:seed

--- db/migrate/20191008182114_create_reviews.rb
+++ db/migrate/20191008182114_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.string :text
       t.references :user, foreign_key: true
-      t.references :review, foreign_key: true
+      t.references :gift, foreign_key: true
 
       t.timestamps
     end
