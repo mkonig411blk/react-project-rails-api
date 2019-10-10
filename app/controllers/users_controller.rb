28341e65683d784e60621a87e935f224f105369b
mkonig411blk <margot.konig@blackrock.com>
2019-10-10 00:50:47 -0500

    added user_controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    def new
+      user = User.new
+    end
+
+    def create
+        user = User.find_or_create_by(email: params[:email])
+        user.name = params[:name]
+        if user.save
+            render json: user, except: [:created_at, :updated_at]
+        else
+            render json: {message: "Signup Failed"}
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+            if user
+            render json: user, except: [:created_at, :updated_at]
+        else
+            render json: {message: "User not found."}
+        end
+    end
+
+private
+    def user_params
+      params.require(:user).permit(:email)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :gifts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show]
 end
